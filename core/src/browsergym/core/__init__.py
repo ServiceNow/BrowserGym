aa033f4bf7ea49843f7c476416c70e4eafe0b721
Maxime Gasse <maxime.gasse@gmail.com>
2024-03-11 15:19:50 -0400

    version bump

--- core/src/browsergym/core/__init__.py
+++ core/src/browsergym/core/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.1a1"
+__version__ = "0.0.1a2"
 
 import playwright.sync_api
 
--- webarena/src/browsergym/webarena/__init__.py
+++ webarena/src/browsergym/webarena/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.1a1"
+__version__ = "0.0.1a2"
 
 from browsergym.core.registration import register_task
 
