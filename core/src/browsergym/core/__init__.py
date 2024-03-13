62d26a169e90bdf048b4b633ebdbea77820b4cf9
Maxime Gasse <maxime.gasse@gmail.com>
2024-03-13 10:36:53 -0400

    version bump

--- core/src/browsergym/core/__init__.py
+++ core/src/browsergym/core/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.1a10"
+__version__ = "0.1.0rc0"
 
 import playwright.sync_api
 
--- miniwob/src/browsergym/miniwob/__init__.py
+++ miniwob/src/browsergym/miniwob/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.1a10"
+__version__ = "0.1.0rc0"
 
 from browsergym.core.registration import register_task
 
