bb0779da979fe8f2fb4c63d7210faece773bf5e5
Maxime Gasse <maxime.gasse@gmail.com>
2024-05-15 16:01:54 -0400

    demo_agent refactor

--- experiments/src/browsergym/experiments/loop.py
+++ experiments/src/browsergym/experiments/loop.py
@@ -1,17 +1,24 @@
 import dataclasses
 
+from abc import abstractmethod
+from browsergym.core.env import BrowserEnv
+
+from .agent import Agent
+
 
 @dataclasses.dataclass
 class EnvArgs:
 
-    def make_env(self):
+    def make_env(self) -> BrowserEnv:
         pass
 
 
 @dataclasses.dataclass
 class AgentArgs:
+    agent_name: str
 
-    def make_env(self):
+    @abstractmethod
+    def make_agent(self) -> Agent:
         pass
 
 
---
+++ experiments/src/browsergym/experiments/utils.py
@@ -0,0 +1,27 @@
+import bs4
+import re
+
+
+def prune_html(html):
+    html = re.sub(r"\n", " ", html)
+    # remove html comments
+    html = re.sub(r"<!--(.*?)-->", "", html, flags=re.MULTILINE)
+
+    soup = bs4.BeautifulSoup(html, "lxml")
+    for tag in reversed(soup.find_all()):
+        # remove body and html tags (not their content)
+        if tag.name in ("html", "body"):
+            tag.unwrap()
+        # remove useless tags
+        elif tag.name in ("style", "link", "script", "br"):
+            tag.decompose()
+        # remove / unwrap structural tags
+        elif tag.name in ("div", "span", "i", "p") and len(tag.attrs) == 1 and tag.has_attr("bid"):
+            if not tag.contents:
+                tag.decompose()
+            else:
+                tag.unwrap()
+
+    html = soup.prettify()
+
+    return html
