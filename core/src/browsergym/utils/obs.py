6c6fa532cecb805f2b187f0fc1770fa70c66db0d
Maxime Gasse <maxime.gasse@gmail.com>
2024-05-16 17:19:26 -0400

    add prune_html to utils.obs

--- core/src/browsergym/utils/obs.py
+++ core/src/browsergym/utils/obs.py
@@ -3,6 +3,7 @@ import numpy as np
 import PIL.Image
 import PIL.ImageDraw
 import PIL.ImageFont
+import re
 
 from collections import defaultdict
 from bs4 import BeautifulSoup
@@ -494,3 +495,28 @@ def overlay_som(
     img = np.array(img)
 
     return img
+
+
+def prune_html(html):
+    html = re.sub(r"\n", " ", html)
+    # remove html comments
+    html = re.sub(r"<!--(.*?)-->", "", html, flags=re.MULTILINE)
+
+    soup = BeautifulSoup(html, "lxml")
+    for tag in reversed(soup.find_all()):
+        # remove body and html tags (not their content)
+        if tag.name in ("html", "body"):
+            tag.unwrap()
+        # remove useless tags
+        elif tag.name in ("style", "link", "script", "br"):
+            tag.decompose()
+        # remove / unwrap structural tags
+        elif tag.name in ("div", "span", "i", "p") and len(tag.attrs) == 1 and tag.has_attr("bid"):
+            if not tag.contents:
+                tag.decompose()
+            else:
+                tag.unwrap()
+
+    html = soup.prettify()
+
+    return html
