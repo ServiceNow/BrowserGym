c95a7fe47d6b94a479d6dfbb7f0853e9957b18c3
ljang0 <ljang@andrew.cmu.edu>
2024-07-10 11:42:59 +0900

    Added/modified files for BrowserGym functionality

--- visualwebarena/src/browsergym/visualwebarena/__init__.py
+++ visualwebarena/src/browsergym/visualwebarena/__init__.py
@@ -1,3 +1,17 @@
 from browsergym.core.registration import register_task
 
-# TODO register the VisualWebArena benchmark
+# register the WebArena benchmark
+from .task import GenericVisualWebArenaTask
+from .config import TASK_IDS
+
+ALL_VISUALWEBARENA_TASK_IDS = []
+
+# register the WebArena benchmark
+for task_id in TASK_IDS:
+    gym_id = f"visualwebarena.{task_id}"
+    register_task(
+        gym_id,
+        GenericVisualWebArenaTask,
+        kwargs={"task_kwargs": {"task_id": task_id}},
+    )
+    ALL_VISUALWEBARENA_TASK_IDS.append(gym_id)
