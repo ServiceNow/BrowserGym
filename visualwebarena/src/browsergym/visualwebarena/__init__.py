a67fbc1ed971bfe5648e3bcacafd47fd8f8a0884
Maxime Gasse <maxime.gasse@gmail.com>
2024-07-17 16:01:37 -0400

    code refactor + tasks with reset

--- visualwebarena/src/browsergym/visualwebarena/__init__.py
+++ visualwebarena/src/browsergym/visualwebarena/__init__.py
@@ -1,17 +1,19 @@
 from browsergym.core.registration import register_task
 
 # register the WebArena benchmark
-from .task import GenericVisualWebArenaTask
-from .config import TASK_IDS
+from . import config, task
 
 ALL_VISUALWEBARENA_TASK_IDS = []
+VISUALWEBARENA_TASK_IDS_WITH_RESET = []
 
 # register the WebArena benchmark
-for task_id in TASK_IDS:
+for task_id in config.TASK_IDS:
     gym_id = f"visualwebarena.{task_id}"
     register_task(
         gym_id,
-        GenericVisualWebArenaTask,
+        task.GenericVisualWebArenaTask,
         kwargs={"task_kwargs": {"task_id": task_id}},
     )
     ALL_VISUALWEBARENA_TASK_IDS.append(gym_id)
+    if task_id in config.TASK_IDS_WITH_RESET:
+        VISUALWEBARENA_TASK_IDS_WITH_RESET.append(gym_id)
