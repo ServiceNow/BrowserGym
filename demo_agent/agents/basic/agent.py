9c036e3d815ae1c542f15aea28c712686441ab0c
Maxime Gasse <maxime.gasse@gmail.com>
2024-09-19 15:42:41 -0400

    demo agent refactor + gpt-4o-mini switch

--- demo_agent/agents/basic/agent.py
+++ demo_agent/agents/basic/agent.py
@@ -10,44 +10,83 @@ class DemoAgent(Agent):
     """A basic agent using OpenAI API, to demonstrate BrowserGym's functionalities."""
 
     action_set = HighLevelActionSet(
-        subsets=["chat", "bid"],  # define a subset of the action space
-        # subsets=["chat", "bid", "coord"] # allow the agent to also use x,y coordinates
+        subsets=["chat", "bid", "infeas"],  # define a subset of the action space
+        # subsets=["chat", "bid", "coord", "infeas"] # allow the agent to also use x,y coordinates
         strict=False,  # less strict on the parsing of the actions
-        multiaction=True,  # enable to agent to take multiple actions at once
+        multiaction=False,  # does not enable the agent to take multiple actions at once
         demo_mode="default",  # add visual effects
     )
     # use this instead to allow the agent to directly use Python code
     # action_set = PythonActionSet())
 
     def obs_preprocessor(self, obs: dict) -> dict:
+
+        # convert legacy text-only goals to structured goals
+        if isinstance(obs["goal"], str):
+            goal = {
+                "text": obs["goal"],
+                "image_urls": [],
+                "image_paths": [],
+            }
+        else:
+            goal = obs["goal"]
+
         return {
-            "goal": obs["goal"],
+            "chat": obs["chat"],
+            "goal": goal,
             "axtree_txt": flatten_axtree_to_str(obs["axtree_object"]),
         }
 
-    def __init__(self, model_name) -> None:
+    def __init__(self, model_name: str, chat_mode: bool) -> None:
         super().__init__()
         self.model_name = model_name
+        self.chat_mode = chat_mode
 
         from openai import OpenAI
 
         self.openai_client = OpenAI()
 
     def get_action(self, obs: dict) -> tuple[str, dict]:
-        system_msg = f"""\
+        if self.chat_mode:
+            system_msg = f"""\
 # Instructions
+
+You are a UI Assistant, your goal is to help the user perform tasks using a web browser. You can
+communicate with the user via a chat, in which the user gives you instructions and in which you
+can send back messages. You have access to a web browser that both you and the user can see,
+and with which only you can interact via specific commands.
+
+Review the instructions from the user, the current state of the page and all other information
+to find the best possible next action to accomplish your goal. Your answer will be interpreted
+and executed by a program, make sure to follow the formatting instructions.
+
+# Chat Messages
+
+"""
+        for msg in obs["chat"]:
+            system_msg += f"""\
+ - [{msg['role']}] {msg['message']}
+"""
+
+        else:
+            system_msg = f"""\
+# Instructions
+
 Review the current state of the page and all other information to find the best
 possible next action to accomplish your goal. Your answer will be interpreted
 and executed by a program, make sure to follow the formatting instructions.
 
-# Goal:
-{obs["goal"]}"""
+# Goal
+
+{obs["goal"]["text"] if obs["goal"] else "The goal is missing."}"""
 
         prompt = f"""\
-# Current Accessibility Tree:
+# Current Accessibility Tree
+
 {obs["axtree_txt"]}
 
 # Action Space
+
 {self.action_set.describe(with_long_description=False, with_examples=True)}
 
 Here is an example with chain of thought of a valid action when clicking on a button:
@@ -79,10 +118,11 @@ class DemoAgentArgs(AbstractAgentArgs):
     internal states of the agent.
     """
 
-    model_name: str = "gpt-3.5-turbo"
+    model_name: str = "gpt-4o-mini"
+    chat_mode: bool = True
 
     def make_agent(self):
-        return DemoAgent(model_name=self.model_name)
+        return DemoAgent(model_name=self.model_name, chat_mode=self.chat_mode)
 
 
 def main():
@@ -93,7 +133,7 @@ def main():
     exp_root.mkdir(exist_ok=True)
 
     exp_args = ExpArgs(
-        agent_args=DemoAgentArgs(model_name="gpt-3.5-turbo"),
+        agent_args=DemoAgentArgs(model_name="gpt-4o-mini"),
         env_args=EnvArgs(
             task_name="miniwob.click-test",
             task_seed=42,
