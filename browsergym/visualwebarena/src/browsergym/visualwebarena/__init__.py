56ead0f7de09cf2b47037001846dcd5a76cdc025
Maxime Gasse <maxime.gasse@gmail.com>
2024-10-01 17:11:37 -0400

    fixed task_kwargs WA / VWA silent bug

--- browsergym/visualwebarena/src/browsergym/visualwebarena/__init__.py
+++ browsergym/visualwebarena/src/browsergym/visualwebarena/__init__.py
@@ -8,12 +8,12 @@ VISUALWEBARENA_TASK_IDS_WITH_RESET = []
 VISUALWEBARENA_TASK_IDS_WITHOUT_RESET = []
 
 # register the WebArena benchmark
-for task_id in config.TASK_IDS:
+for task_id in config.TASK_IDS[:45]:
     gym_id = f"visualwebarena.{task_id}"
     register_task(
         gym_id,
         task.GenericVisualWebArenaTask,
-        kwargs={"task_kwargs": {"task_id": task_id}},
+        task_kwargs={"task_id": task_id},
     )
     ALL_VISUALWEBARENA_TASK_IDS.append(gym_id)
     if task_id in config.TASK_IDS_WITH_RESET:
