a9cfb463c517651ecdb92c78108da5c136ab32ca
Maxime Gasse <maxime.gasse@gmail.com>
2024-11-01 11:27:45 -0400

    version bump v0.11.3

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -6,7 +6,7 @@ project = "BrowserGym"
 copyright = "2024, ServiceNow Research"
 author = "ServiceNow Research"
 
-version = "0.11.2"
+version = "0.11.3"
 release = version
 
 # -- General configuration
