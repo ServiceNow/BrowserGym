732b3dc28cd2cde43ec3e7ab88281627868c485b
Sanjari Srivastava <sanjari@orby.ai>
2025-03-06 18:15:10 -0800

    remove node modules

--- browsergym/subtaskbench/src/browsergym/subtaskbench/package/node_modules/browser-headers/src/index.ts
+++
@@ -1,1 +0,0 @@
-export { BrowserHeaders } from "./BrowserHeaders";
--- browsergym/subtaskbench/src/browsergym/subtaskbench/package/node_modules/hash-wasm/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  roots: [
-    '<rootDir>/test',
-  ],
-  transform: {
-    '^.+\\.ts$': 'ts-jest',
-  },
-  cacheDirectory: '<rootDir>/.jest-cache',
-};
