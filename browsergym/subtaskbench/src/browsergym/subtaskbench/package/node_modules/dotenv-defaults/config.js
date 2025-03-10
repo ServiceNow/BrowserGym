83857f0f6cf3b6353b1a459963d5b79257d28e9e
Sanjari Srivastava <sanjari@orby.ai>
2025-03-09 20:03:13 -0700

    remove innermost nodemodules and try again

--- browsergym/subtaskbench/src/browsergym/subtaskbench/package/node_modules/dotenv-defaults/config.js
+++
@@ -1,7 +0,0 @@
-// The purpose of this file is to be able to load the module with...
-//  ... "require("dotenv-defaults/config")" in JavaScript or "import "dotenv-defaults/config"" in TypeScript...
-//  ... or "node -r dotenv-defaults/config script.js" from the command line
-
-(function () {
-  require('./src/index').config()
-})()
