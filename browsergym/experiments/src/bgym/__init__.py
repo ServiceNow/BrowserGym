5911c1efa0544282d3b31ee2cf28794ffb6627ea
ThibaultLSDC <thibault.de.chezelles@gmail.com>
2025-05-02 15:23:27 -0400

    backtracking

--- browsergym/experiments/src/bgym/__init__.py
+++ browsergym/experiments/src/bgym/__init__.py
@@ -2,6 +2,7 @@ from browsergym.core.action.base import AbstractActionSet
 from browsergym.core.action.highlevel import HighLevelActionSet
 from browsergym.core.action.python import PythonActionSet
 from browsergym.experiments.agent import Agent, AgentInfo
+from browsergym.experiments.benchmark import DEFAULT_BENCHMARKS, Benchmark, HighLevelActionSetArgs
 from browsergym.experiments.loop import (
     AbstractAgentArgs,
     EnvArgs,
