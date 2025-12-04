0019f4e86e6d582609f20cf58ea98c5b2fdf84fd
Nicolas Gontier <gontier.nicolas@yahoo.fr>
2025-12-04 22:01:11 +0000

    compare json with the one in the library

--- browsergym/webarena_verified/src/browsergym/webarena_verified/config.py
+++ browsergym/webarena_verified/src/browsergym/webarena_verified/config.py
@@ -1,3 +1,5 @@
+import difflib
+import importlib.resources
 import json
 from pathlib import Path
 
@@ -6,6 +8,27 @@ INTENT_TEMPLATE_IDS = []
 
 with open(Path(__file__).parent / "webarena_verified.json", "r") as f:
     data = json.load(f)
+
+# Check if the json file is the same as the one in the webarena-verified repository
+library_json_string = (
+    importlib.resources.files("webarena_verified")
+    .joinpath("../../assets/dataset/webarena-verified.json")
+    .read_text()
+)
+library_json = json.loads(library_json_string)
+
+if json.dumps(data, sort_keys=True, indent=2) != json.dumps(library_json, sort_keys=True, indent=2):
+    print(
+        "Warning: the json file is not the same as the one in the webarena-verified repository. Consider updating the library."
+    )
+    print("=" * 100)
+    print("Differences:")
+    for diff in difflib.unified_diff(
+        json.dumps(data, sort_keys=True, indent=2).splitlines(),
+        json.dumps(library_json, sort_keys=True, indent=2).splitlines(),
+    ):
+        print(diff)
+    print("=" * 100)
 
 for task in data:
     INTENT_TEMPLATE_IDS.append(task["intent_template_id"])
